1150a68f2402050affc9c98d266127eebc5cbf47
VladimirGontarenko <mister.gontarenko@yandex.ru>
2023-10-25 00:24:17 +0400

    test change warnings

--- src/main/java/ru/aptech/bustrack/repositories/UserRepository.java
+++ src/main/java/ru/aptech/bustrack/repositories/UserRepository.java
@@ -3,8 +3,6 @@ package ru.aptech.bustrack.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.aptech.bustrack.entities.User;
 
-import java.util.UUID;
-
 public interface UserRepository extends JpaRepository<User, Long> {
     boolean existsByLoginIgnoreCase(String login);
 }
--- src/main/java/ru/aptech/bustrack/services/RoleService.java
+++ src/main/java/ru/aptech/bustrack/services/RoleService.java
@@ -7,9 +7,9 @@ import ru.aptech.bustrack.repositories.RoleRepository;
 
 import java.util.List;
 import java.util.Optional;
-
 @Service
 public class RoleService {
+    @SuppressWarnings("unused")
     @Autowired
     protected RoleRepository roleRepository;
 
