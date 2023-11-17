ef2afd850f253832ab06c60608a4ab30fcb663fb
Arthur <dimmer74sar@gmail.com>
2023-11-17 20:43:19 +0400

    Little work for TransportType

--- src/main/java/ru/aptech/bustrack/entities/TransportType.java
+++ src/main/java/ru/aptech/bustrack/entities/TransportType.java
@@ -16,7 +16,7 @@ public class TransportType {
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "type")
-    private String type;
+    @Column(name = "name")
+    private String name;
 
 }
