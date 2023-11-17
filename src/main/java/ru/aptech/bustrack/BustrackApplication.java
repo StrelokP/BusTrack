b0956f7ea63dfad41f243c2e769962a80db65a22
Markov2023 <andreyakimov43@gmail.com>
2023-11-17 20:23:07 +0400

    test auth

--- src/main/java/ru/aptech/bustrack/BustrackApplication.java
+++ src/main/java/ru/aptech/bustrack/BustrackApplication.java
@@ -6,9 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SuppressWarnings("unused")
 @SpringBootApplication
 public class BustrackApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BustrackApplication.class, args);
 	}
-
 }
